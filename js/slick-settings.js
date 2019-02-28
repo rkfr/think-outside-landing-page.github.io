a0f9be64fdfb31d7a8eec9a1135bc64bd447e0fd
rkfr <e.xbyvnqn@gmail.com>
2019-02-28 20:44:23 +0200

    v.1.1.1 fixed

--- js/slick-settings.js
+++ js/slick-settings.js
@@ -1,29 +1,31 @@
 
-$('.team__slider').slick({
-  centerMode: true,
-  centerPadding: '0',
-  slidesToShow: 3,
-  appendArrows: $('.team__buttons'),
-      prevArrow: '<button class="prev slider-arrow-style" type="button"><div class="prev-slide"></div></button>',
-      nextArrow: '<button class="next slider-arrow-style" type="button"><div class="next-slide"></div></button>',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-      	arrows: true,
-        centerMode: true,
-        centerPadding: '0',
-        slidesToShow: 3
+$(function() {
+  $('.team__slider').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    appendArrows: $('.team__buttons'),
+        prevArrow: '<button class="prev slider-arrow-style" type="button"><div class="prev-slide"></div></button>',
+        nextArrow: '<button class="next slider-arrow-style" type="button"><div class="next-slide"></div></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '0',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0',
+          slidesToShow: 1
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '0',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+    ]
+  });
+})
